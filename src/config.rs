e2b4f3d3accc056e5300944d46ef360aabe98f5c
zhaolin62000 <77498830+zhaolin62000@users.noreply.github.com>
2026-08-03 22:19:31 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -117,7 +117,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.daduo.eu.org"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.huize.eu.org"];
 pub const RS_PUB_KEY: &str = "SAbdXbmkULZxwDbmEFPDoQqMryVkmZ3OXAuBdgr3M=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
