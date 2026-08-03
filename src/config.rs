18768c39106f1e8ec7373fed44c99cf6a59e7092
zhaolin62000 <77498830+zhaolin62000@users.noreply.github.com>
2026-08-03 20:52:53 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.gele.eu.org"];
+pub const RS_PUB_KEY: &str = "F1FDNZ8HS7uFVOGKEiYfP4qRHk9n80OmJnYIH2gMI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
