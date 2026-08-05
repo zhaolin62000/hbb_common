f646e1708ff29db35cd5cf8bcfcd34df50c931d3
zhaolin62000 <77498830+zhaolin62000@users.noreply.github.com>
2026-08-06 03:48:32 +0800

    Update RENDEZVOUS_SERVERS to new address

--- src/config.rs
+++ src/config.rs
@@ -117,7 +117,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.huize.eu.org"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.gele.eu.org"];
 pub const RS_PUB_KEY: &str = "SAbdXbmkULZxwDbmEFPDssoQqMryVkmZ3OXAuBdgr3M=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
